8a7230a9bec0ffe91f3beae294f9e53826fa7395
Alexis <33222808+sathonay@users.noreply.github.com>
2025-05-27 22:19:12 +0200

    header

---
+++ philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alrey <alrey@student.42nice.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 13:53:50 by alrey             #+#    #+#             */
+/*   Updated: 2025/05/27 14:25:45 by alrey            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+long get_current_time_ms();
+
+#endif 
